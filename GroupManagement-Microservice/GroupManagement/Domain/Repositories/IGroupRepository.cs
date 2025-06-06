4d9448b9fd4f22ae5b7731b5cf981047cc5eda29
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-06 15:48:07 -0500

    feat(main):Add domain respositories

---
+++ GroupManagement-Microservice/GroupManagement/Domain/Repositories/IGroupRepository.cs
@@ -0,0 +1,12 @@
+﻿using DittoBox.API.GroupManagement.Domain.Models.Entities;
+using DittoBox.API.Shared.Domain.Repositories;
+
+
+namespace DittoBox.API.GroupManagement.Domain.Repositories
+{
+    public interface IGroupRepository : IBaseRepository<Group>
+    {
+        public Task<IEnumerable<int>> GetProfileCountByGroupId(int groupId);
+        public Task<IEnumerable<int>> GetContainerCountByGroupId(int groupId);
+    }
+}
